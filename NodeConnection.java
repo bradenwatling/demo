0625c70047093123cee8bb6c5a6277b9582eefc3
Braden Watling <bradenwatling@hotmail.com>
2014-06-24 16:42:54 -0400

    Create NodeConnection.java

---
+++ NodeConnection.java
@@ -0,0 +1,11 @@
+public class NodeConnection {
+  private Data mSource;
+  private Data mTarget;
+  
+  public NodeConnection(Data source, Data target) {
+    mSource = source;
+    mTarget = target;
+  }
+  
+  
+}
